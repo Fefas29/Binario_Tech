cdbe9dc0521e91d468ef3c1875b3a881f9f4373b
Felipe Rosa da Silva <silvafeliperosa@gmail.com>
2026-09-18 17:10:33 +0000

    fq

--- binario_tech/aula16/auditoria_servidor.sh
+++ binario_tech/aula16/auditoria_servidor.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
 
-echo "================================"
-echo " AUDITORIA DE PROCESSOS NODE.JS "
-echo "================================"
-
 ps aux | grep node > processos.log
-
-echo "Auditoria concluída!"
-echo "Resultado salvo em processos.log"
